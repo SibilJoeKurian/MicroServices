0da289d884bc08e95da9565b3badea9d2fd4a7f7
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2020-08-28 23:59:34 -0400

    docker extension

--- movie_info_service/src/main/java/com/example/movie_info_service/MovieInfoServiceApplication.java
+++ movie_info_service/src/main/java/com/example/movie_info_service/MovieInfoServiceApplication.java
@@ -1,12 +1,17 @@
 package com.example.movie_info_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class MovieInfoServiceApplication {
-
+	public static Logger logger= LoggerFactory.getLogger(MovieInfoServiceApplication.class);
 	public static void main(String[] args) {
+		logger.info("In spring application");
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
 
--- movie_info_service/src/test/java/com/example/movie_info_service/MovieInfoServiceApplicationTests.java
+++ movie_info_service/src/test/java/com/example/movie_info_service/MovieInfoServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.movie_info_service;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieInfoServiceApplicationTests {
-
+public static Logger logger1= LoggerFactory.getLogger(MovieInfoServiceApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger1.info("Test started");
 	}
-
 }
