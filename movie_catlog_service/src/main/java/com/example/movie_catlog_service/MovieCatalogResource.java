bcc6f3eb1e8573c272074bda2851fc4c6ea3cb30
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2020-09-02 23:44:06 -0400

    adding REST Template

--- movie_catlog_service/src/main/java/com/example/movie_catlog_service/MovieCatalogResource.java
+++ movie_catlog_service/src/main/java/com/example/movie_catlog_service/MovieCatalogResource.java
@@ -1,11 +1,15 @@
 package com.example.movie_catlog_service;
 
 import Model.CatalogItem;
+import Model.Movie;
+import Model.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.Arrays.*;
 import java.util.Collections;
 import java.util.List;
 //http://localhost:8080/catalog/abc
@@ -14,7 +18,16 @@ import java.util.List;
 public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")String userId){
+
+        List<Rating> ratings;
+        ratings =  Arrays.asList(new Rating("1234",4),new Rating("2345",3));
         RestTemplate restTemplate=new RestTemplate();
+        Movie movie;
+        for(int i=0;i<ratings.size();i++){
+            String movieId=ratings.get(i).getMovieId();
+            movie= restTemplate.getForObject("http://localhost:8081/movies/"+movieId, Movie.class);
+
+        }
 
         return Collections.singletonList(new CatalogItem("Transformers","Sci-Fi",4));
     }
